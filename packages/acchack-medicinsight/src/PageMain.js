022662a565df03b500f2921e2ad8159dac6657f6
Frank Wijmans <fswijmans@gmail.com>
2019-10-04 15:26:25 +0200

    WIP2

--- packages/acchack-medicinsight/src/PageMain.js
+++ packages/acchack-medicinsight/src/PageMain.js
@@ -1,16 +1,12 @@
-import {
-  html,
-  css,
-  LitElement
-} from 'lit-element';
+import { html, css, LitElement } from 'lit-element';
 
-import horizonData from "../../../utils/processJSON.js";
+import horizonData from '../../../utils/processJSON.js';
 
 import '../checkbox-selector.js';
 
 export class PageMain extends LitElement {
   static get styles() {
-    return css `
+    return css`
       checkbox-selector {
         margin: 4px;
       }
@@ -32,15 +28,24 @@ export class PageMain extends LitElement {
         type: Array,
       },
       data: {
+<<<<<<< HEAD
         type: Array
       },
       dataView: {
         type: Array
       }
+=======
+        type: Array,
+      },
+      dataView: {
+        type: Array,
+      },
+>>>>>>> WIP2
     };
   }
 
   constructor() {
+<<<<<<< HEAD
     super()
 
     this.data = horizonData.slice(0, 10);
@@ -110,7 +115,91 @@ export class PageMain extends LitElement {
 
     <!-- Magic here with domains and company -->
   ${this.dataView.map((item, index) => html `<p>${index+1}. ${item.Stofnaam}</p>`)}
+=======
+    super();
 
-        `;
+    this.data = horizonData.slice(0, 10);
+    this.domains = this.loadDomains();
+    this.domainsSelected = [];
+    this.company = PageMain.getFabrikant(this.loadCompanies());
+    this.companySelected = [];
+    this.dataView = this._updateDataView();
+  }
+
+  static getFabrikant(set) {
+    const _selectFabrikant = item => item.Fabrikant;
+    return set.map(_selectFabrikant);
+  }
+
+  loadDomains() {
+    const _isEmpty = value => value !== '';
+    const _isDistinct = (value, index, self) => self.indexOf(value) === index;
+    const _selectDomain = item => item.Domein;
+    return this.data
+      .map(_selectDomain)
+      .filter(_isEmpty)
+      .filter(_isDistinct)
+      .sort();
+  }
+
+  loadCompanies() {
+    const _isEmpty = value => value !== '';
+    const _isDistinct = (value, index, self) => self.indexOf(value) === index;
+
+    return this.data
+      .filter(_isEmpty)
+      .filter(_isDistinct)
+      .sort();
+  }
+>>>>>>> WIP2
+
+  _updateDomain(event) {
+    this.domainsSelected = event.detail;
+    // console.log(this.domainsSelected, this.domainsSelected.indexOf());
+    this.dataView = this._updateDataView();
+
+    if (this.domainsSelected.length > 0) {
+      const _isSelectedDomain = value =>
+        // console.log(value, this.domainsSelected);
+        this.domainsSelected.indexOf(value.Domein) !== -1;
+
+      this.company = PageMain.getFabrikant(this.loadCompanies().filter(_isSelectedDomain));
+    } else {
+      this.company = PageMain.getFabrikant(this.loadCompanies());
+    }
+  }
+
+  _updateCompany(event) {
+    this.domainsSelected = event.detail;
+    this._updateDataView();
+  }
+
+  _updateDataView() {
+    return this.data;
+  }
+
+  render() {
+    return html`
+      <checkbox-selector
+        .values="${this.domains}"
+        .selected="${this.domainsSelected}"
+        title="Domain"
+        @update="${this._updateDomain}"
+      ></checkbox-selector>
+      <checkbox-selector
+        .values="${this.company}"
+        .selected="${this.companySelected}"
+        title="Company"
+        @update="${this._updateCompany}"
+      ></checkbox-selector>
+
+      <!-- Magic here with domains and company -->
+      ${this.dataView.map(
+        (item, index) =>
+          html`
+            <p>${index + 1}. ${item.Stofnaam}</p>
+          `,
+      )}
+    `;
   }
 }
--- packages/acchack-medicinsight/src/Selector.js
+++ packages/acchack-medicinsight/src/Selector.js
@@ -1,77 +1,83 @@
-import {
-    html,
-    css,
-    LitElement
-} from 'lit-element';
+import { html, css, LitElement } from 'lit-element';
 
 export class Selector extends LitElement {
-    static get styles() {
-        return css `
-      .list{
+  static get styles() {
+    return css`
+      .list {
         display: flex;
         flex-direction: column;
       }
-  
-      .group{
+
+      .group {
         border: 1px solid black;
         border-radius: 4px;
-        padding: 8px; 
+        padding: 8px;
       }
-  
+
       .group h2 {
         font-size: 16px;
       }
-  
-  
-      `;
+    `;
+  }
+
+  static get properties() {
+    return {
+      values: {
+        type: Array,
+      },
+      selected: {
+        type: Array,
+      },
+      title: {
+        type: String,
+      },
+    };
+  }
+
+  constructor() {
+    super();
+    this.values = [];
+    this.selected = [];
+  }
+
+  _clickValue(e) {
+    const {
+      target: { value },
+    } = e;
+    if (this.selected.indexOf(value) === -1) {
+      this.selected.push(value);
+    } else {
+      console.log('hi');
+      this.selected = this.selected.filter(item => item !== value);
     }
 
-    static get properties() {
-        return {
-            values: {
-                type: Array,
-            },
-            selected: {
-                type: Array,
-            },
-            title: {
-                type: String,
-            }
+    this.dispatchEvent(
+      new CustomEvent('update', {
+        detail: this.selected,
+      }),
+    );
+  }
 
-        };
-    }
-
-    constructor() {
-        super()
-        this.values = [];
-        this.selected = [];
-    }
-
-    _clickValue(e) {
-        const {
-            target: {
-                value
-            }
-        } = e;
-        if (this.selected.indexOf(value) === -1) {
-            this.selected.push(value);
-        } else {
-            this.selected = this.selected.filter(item => item !== value)
-        }
-
-        console.log(`Selected ${this.title}: ${this.selected}`);
-
-    }
-
-    render() {
-        return html `<div class="group">
-            <h2>${this.title}</h2>
-            <div class="list">
-          ${this.values.map(element => 
-            html `<label><input type="checkbox" value="${element}" name="${this.title}" @click="${this._clickValue}">${element}</label>`
+  render() {
+    return html`
+      <div class="group">
+        <h2>${this.title}</h2>
+        <div class="list">
+          ${this.values.map(
+            element =>
+              html`
+                <label
+                  ><input
+                    type="checkbox"
+                    value="${element}"
+                    name="${this.title}"
+                    @click="${this._clickValue}"
+                  />${element}</label
+                >
+              `,
           )}
-          </div>
-          </div>
-          `;
-    }
+        </div>
+      </div>
+    `;
+  }
 }
